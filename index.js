16b979efa3f869235972d339d01fe52b331d249d
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-05-01 23:33:32 +0600

    mongodb conected

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const cors = require('cors');
 const app=express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+require('dotenv').config()
 const port =process.env.PORT || 5000 ;
+
 app.use(cors());
 app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.vqMOj7AIqtHCWKsY}@cluster0.gdyfp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log('mongo db is connect')
+  client.close();
+});
+
 
 
 app.get('/',(req,res)=>{
