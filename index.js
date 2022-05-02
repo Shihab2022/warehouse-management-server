f35794e4a1569a02ca1f1fece563e82f16ad7aaf
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-05-02 11:19:51 +0600

    find product by id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app=express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion , ObjectId } = require('mongodb');
 
 require('dotenv').config()
 const port =process.env.PORT || 5000 ;
@@ -28,8 +28,19 @@ async function run() {
     const product=await cursor.toArray();
     res.send(product);
    })
+
+   app.get("/products/:id", async (req, res) => {
+    const id = req.params.id;
+    const query={_id: ObjectId(id)};
+    const result = await carCollection.findOne(query);
+    console.log(result)
+    res.send(result);
+
+   })
+
+
+  
   }
- 
 
 finally{}
 }
