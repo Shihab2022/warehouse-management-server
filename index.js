e5d4832abcb89af4488663ee7874b1c1c595d61b
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-05-05 19:37:13 +0600

    implement delete api

--- index.js
+++ index.js
@@ -1,74 +1,76 @@
-const express = require('express');
-const cors = require('cors');
-const app=express();
-const { MongoClient, ServerApiVersion , ObjectId } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-require('dotenv').config()
-let port =process.env.PORT || 5000 ;
+require("dotenv").config();
+let port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gdyfp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
-
   try {
     await client.connect();
     const carCollection = client.db("royal-cars").collection("cars");
 
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = carCollection.find(query);
+      const product = await cursor.toArray();
+      res.send(product);
+    });
 
-   app.get('/products',async(req, res) => {
-    const query = {};
-    const cursor = carCollection.find(query);
-    const product=await cursor.limit(6).toArray();
-    res.send(product);
-   })
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await carCollection.findOne(query);
+      console.log(result);
+      res.send(result);
+    });
+    app.put("/products/:id", async (req, res) => {
+      //  console.log(req)
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updatedQuantity = req.body;
+      console.log(updatedQuantity);
+      //  res.send(updatedUser)
 
-   app.get("/products/:id", async (req, res) => {
-    const id = req.params.id;
-    const query={_id: ObjectId(id)};
-    const result = await carCollection.findOne(query);
-    console.log(result)
-    res.send(result);
+      const updatedDoc = {
+        $set: {
+          quantity: updatedQuantity.updateQuantity2,
+        },
+      };
+      const result = await carCollection.updateOne(filter, updatedDoc, options);
+      res.send(result);
+    });
 
-   })
-   app.put('/products/:id', async (req, res) => {
-    //  console.log(req)
-     const id = req.params.id;
-     const filter={_id: ObjectId(id)};
-     const options = { upsert: true };
-     const updatedQuantity = req.body;
-     console.log(updatedQuantity)
-    //  res.send(updatedUser)
+    app.post("/products", async (req, res) => {
+      const newUser = req.body;
+      console.log("ok", req.body);
+      const result = await carCollection.insertOne(newUser);
 
-     const updatedDoc = {
-         $set: {
-          quantity: updatedQuantity.updateQuantity2
-         }
-     };
-     const result = await carCollection.updateOne(filter, updatedDoc, options);
-     res.send(result);
-   })
+      res.send(result);
+    });
 
-app.post('/products', async (req, res)=>{
-  const newUser=req.body;
-  console.log('ok' ,req.body)
-  const result = await carCollection.insertOne(newUser);
-
-res.send(result);
-})
-
+    app.delete('/products/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const result = await carCollection.deleteOne(query);
+      res.send(result);
+  })
 
 
-  
+  } finally {
   }
-
-finally{}
 }
 run().catch(console.dir);
 
@@ -79,12 +81,9 @@ run().catch(console.dir);
 //   client.close();
 // });
 
-
-
-app.get('/',(req,res)=>{
-
-    res.send('start project')
-})
-app.listen(port ,()=>{
-    console.log('listening on port' , port)
-})
+app.get("/", (req, res) => {
+  res.send("start project");
+});
+app.listen(port, () => {
+  console.log("listening on port", port);
+});
